94b7da3086569d7f1c5bc75fc933615e7a7307f4
Sadiq Salau <sadiqsalau888@gmail.com>
2021-09-19 08:46:05 -0700

    Removed the config option USE_INDEX_HTML_FOR_DIR

--- web/config.js
+++ web/config.js
@@ -3,8 +3,4 @@ module.exports = {
     WEB_PATH:           path.join(__dirname, 'public'), // The web directory
     HEADER_TEMPLATE:    path.join(__dirname, "WebFiles.example.h"), // The header template
     HEADER_OUTPUT:      path.join(__dirname, "../WebFiles.h"), // The header output path
-    /**
-     * Replace requests to e.g /home to /home/index.html if /home/index.html exists 
-     */
-    USE_INDEX_HTML_FOR_DIR: true
 };
