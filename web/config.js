ff0c540dc1f5ba98b47d3bb0bcc334f775526546
Sadiq Salau <sadiqsalau888@gmail.com>
2021-09-21 11:53:58 -0700

    Cleanups

--- web/config.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-module.exports = {
-    WEB_PATH:           path.join(__dirname, 'public'), // The web directory
-    HEADER_TEMPLATE:    path.join(__dirname, "WebFiles.example.h"), // The header template
-    HEADER_OUTPUT:      path.join(__dirname, "../WebFiles.h"), // The header output path
-};
